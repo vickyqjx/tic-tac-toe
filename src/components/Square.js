a4468e5571086652d16becbc2c6dcfe6337c26fb
Jingxian Qiu <jingxianqiu@jingxians-mbp.home>
2020-04-14 16:46:34 +1000

    UI updates

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 function Square(props) {
-  const className = props.ishighlighted ? "square highlighted" : "square";
+  let className = props.ishighlighted ? "square highlighted" : "square";
+  className = props.value === null ? `${className} active` : className;
   return (
     <button className={className} onClick={() => props.onClick()}>
       {props.value}
