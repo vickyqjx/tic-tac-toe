27adce4059393d5da7c406b57171f1cfe6cee9b1
Jingxian Qiu <jingxianqiu@jingxians-mbp.home>
2020-04-14 14:23:31 +1000

    remove unused file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Board from './components/Board.js';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
+function App() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
       </div>
-    );
-  }
+      <div className="game-info">
+        <div>{/* status */}</div>
+        <ol>{/* TODO */}</ol>
+      </div>
+    </div>
+  );
 }
 
 export default App;
