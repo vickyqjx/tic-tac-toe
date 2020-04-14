bd8985e73f987ed6c038f5f488ac15130f724933
Jingxian Qiu <jingxianqiu@jingxians-mbp.home>
2020-04-14 15:16:01 +1000

    Updates

---
+++ precache-manifest.96ad731c7b46c7dcd5c1a6ce9f4f3342.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac2b33a88e1e965a1c8523ceb1e9c06c",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "46ade59e9d94a811c4a1",
+    "url": "/tic-tac-toe/static/css/main.2afb2ac7.chunk.css"
+  },
+  {
+    "revision": "71a4d3df0b73dda42021",
+    "url": "/tic-tac-toe/static/js/2.ab9b7b88.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.ab9b7b88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46ade59e9d94a811c4a1",
+    "url": "/tic-tac-toe/static/js/main.fedc89b7.chunk.js"
+  },
+  {
+    "revision": "b83c3f5dfddd51f16016",
+    "url": "/tic-tac-toe/static/js/runtime-main.e9088102.js"
+  }
+]);
